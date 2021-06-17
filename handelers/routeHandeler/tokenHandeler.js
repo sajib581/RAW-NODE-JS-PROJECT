f88e9ec848aee7f8606e019ec62fc3253ee57ce1
sajib581 <sajibdas581@gmail.com>
2021-06-17 23:29:25 +0600

    user  crud with token

--- handelers/routeHandeler/tokenHandeler.js
+++ handelers/routeHandeler/tokenHandeler.js
@@ -117,4 +117,18 @@ const {
     }
    };
 
+   handeler._token.verify = (id, phone, callback) => {
+       read('tokens', id, (err, tokenData) => {
+           if (!err && tokenData) {
+                if (parseJSON(tokenData).phone === phone && parseJSON(tokenData).expires > Date.now()) {
+                    callback(true);
+                } else {
+                    callback(false);
+                }
+           } else {
+               callback(false);
+           }
+       });
+   };
+
    module.exports = handeler;
--- handelers/routeHandeler/userHandeler.js
+++ handelers/routeHandeler/userHandeler.js
@@ -1,140 +1,193 @@
+/* eslint-disable max-len */
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-underscore-dangle */
 
 const {
  read, create, update, deletes,
 } = require('../../lib/data');
+const tokenHandeler = require('./tokenHandeler');
 const { hassPass, parseJSON } = require('../../utilities/utilities');
 
 const handeler = {};
 handeler.userHandeler = (requestProperties, callback) => {
-    const acceeptedMethod = ['post', 'get', 'put', 'delete'];
-    if (acceeptedMethod.indexOf(requestProperties.method) > -1) {
-        handeler._user[requestProperties.method](requestProperties, callback);
-    } else {
-        callback(405);
-    }
+  const acceeptedMethod = ['post', 'get', 'put', 'delete'];
+  if (acceeptedMethod.indexOf(requestProperties.method) > -1) {
+    handeler._user[requestProperties.method](requestProperties, callback);
+  } else {
+    callback(405);
+  }
 };
 
 handeler._user = {};
 
 handeler._user.post = (requestProperties, callback) => {
-let {
+  let {
  firstName, lastName, phone, password, tosAgreement,
 } = requestProperties.body;
-    firstName = typeof firstName === 'string' && firstName.trim().length > 0 ? firstName : false;
-    lastName = typeof lastName === 'string' && lastName.trim().length > 0 ? lastName : false;
-    phone = typeof phone === 'string' && phone.trim().length === 11 ? phone : false;
-    password = typeof password === 'string' && password.trim().length > 0 ? password : false;
-    tosAgreement = typeof tosAgreement === 'boolean' && tosAgreement ? tosAgreement : false;
+  firstName = typeof firstName === 'string' && firstName.trim().length > 0
+      ? firstName
+      : false;
+  lastName = typeof lastName === 'string' && lastName.trim().length > 0
+      ? lastName
+      : false;
+  phone = typeof phone === 'string' && phone.trim().length === 11 ? phone : false;
+  password = typeof password === 'string' && password.trim().length > 0
+      ? password
+      : false;
+  tosAgreement = typeof tosAgreement === 'boolean' && tosAgreement ? tosAgreement : false;
 
-    if (firstName && lastName && phone && password && tosAgreement) {
-        read('users', 'phone', (err1) => {
-            if (err1) {
-                const userObject = {
-                                firstName,
-                                lastName,
-                                phone,
-                                password: hassPass(password),
-                                tosAgreement,
-                            };
-                            create('users', phone, userObject, (err2) => {
-                                if (!err2) {
-                                    callback(200, { message: 'User created Successsfully' });
-                                } else {
-                                    callback(500, { message: 'could not create user' });
-                                }
-                            });
-            } else {
-                callback(500, { message: 'Server Errror ccured' });
-            }
+  if (firstName && lastName && phone && password && tosAgreement) {
+    read('users', 'phone', (err1) => {
+      if (err1) {
+        const userObject = {
+          firstName,
+          lastName,
+          phone,
+          password: hassPass(password),
+          tosAgreement,
+        };
+        create('users', phone, userObject, (err2) => {
+          if (!err2) {
+            callback(200, { message: 'User created Successsfully' });
+          } else {
+            callback(500, { message: 'could not create user' });
+          }
         });
-    } else {
-        callback('400', { message: 'USER input Problem' });
-    }
+      } else {
+        callback(500, { message: 'Server Errror ccured' });
+      }
+    });
+  } else {
+    callback('400', { message: 'USER input Problem' });
+  }
 };
 
 // @TODO: Authentication
 handeler._user.get = (requestProperties, callback) => {
-    const phone = requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
-    if (phone) {
+  const phone = requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+  if (phone) {
+    // verify token
+    const token = typeof requestProperties.headersObject.token === 'string'
+        ? requestProperties.headersObject.token
+        : false;
+    tokenHandeler._token.verify(token, phone, (idToken) => {
+      if (idToken) {
+        // Look up the user
         read('users', phone, (err, u) => {
-            const user = { ...parseJSON(u) };
-            if (!err) {
-                delete user.password;
-                callback(200, user);
-            } else {
-        callback('500', { message: 'Server Errror' });
-            }
+          const user = { ...parseJSON(u) };
+          if (!err) {
+            delete user.password;
+            callback(200, user);
+          } else {
+            callback('500', { message: 'Server Errror' });
+          }
         });
-    } else {
-        callback('404', { message: 'Requested User does not exist' });
-    }
+      } else {
+        callback('403', { message: 'Authentication Failed' });
+      }
+    });
+    // end
+  } else {
+    callback('404', { message: 'Requested User does not exist' });
+  }
 };
 
 // @TODO: Authentication
 handeler._user.put = (requestProperties, callback) => {
-    let {
-        firstName, lastName, phone, password,
-       } = requestProperties.body;
-           firstName = typeof firstName === 'string' && firstName.trim().length > 0 ? firstName : false;
-           lastName = typeof lastName === 'string' && lastName.trim().length > 0 ? lastName : false;
-           phone = typeof phone === 'string' && phone.trim().length === 11 ? phone : false;
-           password = typeof password === 'string' && password.trim().length > 0 ? password : false;
+  let {
+ firstName, lastName, phone, password,
+} = requestProperties.body;
+  firstName = typeof firstName === 'string' && firstName.trim().length > 0
+      ? firstName
+      : false;
+  lastName = typeof lastName === 'string' && lastName.trim().length > 0
+      ? lastName
+      : false;
+  phone = typeof phone === 'string' && phone.trim().length === 11 ? phone : false;
+  password = typeof password === 'string' && password.trim().length > 0
+      ? password
+      : false;
 
-           if (phone) {
-                if (firstName || lastName || password) {
-                    read('users', phone, (err1, data) => {
-                        const userData = { ...parseJSON(data) };
-                        if (!err1 && userData) {
-                            if (firstName) {
-                                userData.firstName = firstName;
-                            }
-                            if (lastName) {
-                                userData.lastName = lastName;
-                            }
-                            if (password) {
-                                userData.password = hassPass(password);
-                            }
-                            update('users', phone, userData, (err2) => {
-                                if (!err2) {
-                                    callback(200, { message: 'user updated successfully' });
-                                } else {
-                                    callback(500, { message: 'Server Error occurred' });
-                                }
-                            });
-                        } else {
-                            callback(404, { message: 'File not found' });
-                        }
-                    });
-                } else {
-                    callback(404, { message: 'Null Input ' });
-                }
-           } else {
-               callback(404, 'phone number syntex error');
-           }
+  if (phone) {
+    if (firstName || lastName || password) {
+      // verify token
+    const token = typeof requestProperties.headersObject.token === 'string'
+    ? requestProperties.headersObject.token
+    : false;
+tokenHandeler._token.verify(token, phone, (idToken) => {
+  if (idToken) {
+    // Look up the user
+    read('users', phone, (err1, data) => {
+      const userData = { ...parseJSON(data) };
+      if (!err1 && userData) {
+        if (firstName) {
+          userData.firstName = firstName;
+        }
+        if (lastName) {
+          userData.lastName = lastName;
+        }
+        if (password) {
+          userData.password = hassPass(password);
+        }
+        update('users', phone, userData, (err2) => {
+          if (!err2) {
+            callback(200, { message: 'user updated successfully' });
+          } else {
+            callback(500, { message: 'Server Error occurred' });
+          }
+        });
+      } else {
+        callback(404, { message: 'File not found' });
+      }
+    });
+  } else {
+    callback('403', { message: 'Authentication Failed' });
+  }
+});
+    } else {
+      callback(404, { message: 'Null Input ' });
+    }
+  } else {
+    callback(404, 'phone number syntex error');
+  }
 };
 
 // @TODO Authentication
 handeler._user.delete = (requestProperties, callback) => {
-    const phone = requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
-       if (phone) {
+  const phone = requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+  if (phone) {
+    // verify token
+    const token = typeof requestProperties.headersObject.token === 'string'
+        ? requestProperties.headersObject.token
+        : false;
+    tokenHandeler._token.verify(token, phone, (idToken) => {
+      if (idToken) {
+        // Look up the user
         read('users', phone, (err, userData) => {
-            if (!err && userData) {
-                deletes('users', phone, (err2) => {
-                    if (!err2) {
-                        callback(200, { message: 'User deleted successfully' });
-                    } else {
-                        callback(500, { message: 'Server error' });
-                    }
-                });
-            } else {
+          if (!err && userData) {
+            deletes('users', phone, (err2) => {
+              if (!err2) {
+                callback(200, { message: 'User deleted successfully' });
+              } else {
                 callback(500, { message: 'Server error' });
-            }
+              }
+            });
+          } else {
+            callback(500, { message: 'Server error' });
+          }
         });
-       } else {
-           callback(404, { message: 'invalid request' });
-       }
+      } else {
+        callback('403', { message: 'Authentication Failed' });
+      }
+    });
+    // end
+  } else {
+    callback(404, { message: 'invalid request' });
+  }
 };
 
 module.exports = handeler;
